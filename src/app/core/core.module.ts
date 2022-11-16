a74c1b599f9dac950b871bae7d754178395c84d4
Amato <jeanphainaut@gmail.com>
2022-11-16 12:05:36 +0100

    Adding component and planning

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
-    BrowserAnimationsModule,
+    BrowserAnimationsModule,  
     PublicModule,
     ProtectedModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class CoreModule {
